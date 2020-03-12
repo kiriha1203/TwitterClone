ae0155b60ba0406b584fa2c0cd54556de4fdfce2
takefumi mizumoto <mizumoto.takefumi@gmail.com>
2020-03-12 11:12:43 +0900

    create edit

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -14,9 +14,13 @@ class TweetsController < ApplicationController
   end
 
   def edit
+    @tweet = Tweet.find(params[:id])
   end
 
   def update
+    tweet = Tweet.find(params[:id])
+    tweet.update!(tweet_params)
+    redirect_to tweets_url, notice: "「#{tweet.created_at}のツイートを更新しました。」"
   end
 
   def destroy
